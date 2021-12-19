81389d10c6a6e74fb3745a51f1720c1e4842e6b8
pallabez <pallabsonowal999@gmail.com>
2021-12-19 12:57:02 +0530

    Added user_profile view

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 module.exports.profile = function(req, res) {
-    return res.end("<h1>User profile</h1>")
+    return res.render('user_profile', {
+        title: "Profile",
+    });
 }
 
 module.exports.post = function(req, res) {
