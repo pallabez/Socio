713771e86f543e717d837d35fb357ce64e4a58d5
pallabez <pallabsonowal999@gmail.com>
2022-02-09 09:48:20 +0530

    add profile change option

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,9 +5,19 @@ module.exports.profile = function(req, res) {
     User.findById(req.params.id, function(err, user) {
         return res.render('user_profile', {
             title: 'User Profile',
-            profile_user: user,
+            profileUser: user,
         });
     });
+}
+
+module.exports.update = function(req, res) {
+    if(req.user.id == req.params.id) {
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user) {
+            return res.redirect('back');
+        });
+    } else {
+        return res.status(401).send('Unauthorized');
+    }
 }
 
 module.exports.signUp = function(req, res) {
